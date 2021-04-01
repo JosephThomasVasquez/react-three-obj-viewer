ea19816cc03fb50f691125fc072c50b9d144b267
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-03-31 20:51:08 -0700

    Added initialize firestore app code

---
+++ server/uploadData.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firestore-export-import";
+
+import serviceAccount from "./serviceAccountKey.json";
+
+// If you want to pass settings for firestore, you can add to the options parameters
+const options = {
+  firestore: {
+    host: "localhost:8080",
+    ssl: false,
+  },
+};
+
+// Initiate Firebase App
+// appName is optional, you can omit it.
+const appName = "[DEFAULT]";
+initializeApp(serviceAccount, appName, options);
+
+// the appName & options are OPTIONAL
+// you can initalize the app without them
+// initializeApp(serviceAccount)
