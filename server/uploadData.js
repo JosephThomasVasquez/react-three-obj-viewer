abd90af1f964e86adbf443b040022ec6f23b6c48
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-03-31 21:51:30 -0700

    Added data folder and data.json file for starter test data

--- server/uploadData.js
+++ server/uploadData.js
@@ -1,6 +1,5 @@
-import firestoreService from "firestore-export-import";
-
-import serviceAccount from "./serviceAccountKey.json";
+const firestoreService = require("firestore-export-import");
+const serviceAccount = require("./fbCred.json");
 
 // If you want to pass settings for firestore, you can add to the options parameters
 const options = {
@@ -12,9 +11,11 @@ const options = {
 
 const db = "";
 
+const appName = "react-three-obj-viewer";
+
 // Initiate Firebase App
-firestoreService.initializeApp(serviceAccount, options);
-firestoreService.restore("data.json");
+firestoreService.initializeApp(serviceAccount);
+firestoreService.restore('./data/data.json');
 
 // the appName & options are OPTIONAL
 // you can initalize the app without them
