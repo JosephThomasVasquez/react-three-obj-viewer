6452e0e3a7941a74588c73fdddcc7f9ce2e316c3
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-02 09:59:11 -0700

    Created the Progress Bar component

---
+++ client/src/components/ProgressBar.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import UseFBStorage from "./firebaseComponents/UseFBStorage";
+
+const ProgressBar = () => {
+  return <div></div>;
+};
+
+export default ProgressBar;
--- client/src/components/firebaseComponents/UseFBStorage.js
+++ client/src/components/firebaseComponents/UseFBStorage.js
@@ -2,8 +2,33 @@ import React, { useState, useEffect } from "react";
 import ErrorMsg from "../ErrorMsg";
 import { appStorage } from "../../firebase/config";
 
-const UseFBStorage = () => {
-  return <div></div>;
+const UseFBStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState("");
+  const [fileUrl, setFileUrl] = useState("");
+
+  useEffect(() => {
+    // References
+    const storageReference = appStorage.ref(file.name);
+
+    // uploads the file to FireBase storage
+    storageReference.put().on(
+      "state_changed",
+      (snap) => {
+        let percentage = (snap.bytesTransfered / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (error) => {
+        setError(error);
+      },
+      async () => {
+        const url = await storageReference.getDownloadURL();
+        setFileUrl(url);
+      }
+    );
+  }, [file]);
+
+  return { progress, fileUrl, error };
 };
 
 export default UseFBStorage;
