77a4b31d1b4487854de9947ecdc2f186d2a9727b
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-01 21:16:52 -0700

    Fixed styling

---
+++ client/src/views/HomeView.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import ImagesGrid from "./components/ImagesGrid";
+
+const HomeView = () => {
+  return (
+    <div>
+      <Header />
+      <ImagesGrid />
+    </div>
+  );
+};
+
+export default HomeView;
