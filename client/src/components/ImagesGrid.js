4126f4adf8f591674f42387e8687ab135ca3f852
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-01 17:24:45 -0700

    Set margin to a css variable and updating styling of grit items

--- client/src/components/ImagesGrid.js
+++ client/src/components/ImagesGrid.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const ImagesGrid = () => {
-    return (
-        <div className="grid-container">
-            <picture className="image-grid-item"></picture>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="grid-container">
+        <picture className="image-grid-item"></picture>
+        <picture className="image-grid-item"></picture>
+        <picture className="image-grid-item"></picture>
+        <picture className="image-grid-item"></picture>
+      </div>
+    </div>
+  );
+};
 
-export default ImagesGrid
+export default ImagesGrid;
