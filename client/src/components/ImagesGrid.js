a5ed5e9dcc5915b11027b526b40797badae1bdb4
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-01 17:51:39 -0700

    Added mroe grid styling and shadows with image sizes

--- client/src/components/ImagesGrid.js
+++ client/src/components/ImagesGrid.js
@@ -4,10 +4,150 @@ const ImagesGrid = () => {
   return (
     <div className="container">
       <div className="grid-container">
-        <picture className="image-grid-item"></picture>
-        <picture className="image-grid-item"></picture>
-        <picture className="image-grid-item"></picture>
-        <picture className="image-grid-item"></picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
+        <picture className="image-grid-item">
+          <source
+            media="(min-width:650px)"
+            srcset="https://source.unsplash.com/random"
+          />
+          <img
+            src="https://source.unsplash.com/random"
+            alt="random image"
+            className="grid-img"
+          />
+          <section className="grid-image-title">This is the Title</section>
+        </picture>
       </div>
     </div>
   );
