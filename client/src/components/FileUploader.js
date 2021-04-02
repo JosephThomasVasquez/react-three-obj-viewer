a04a71aaa81badac9742a4bef4493b99dcb09749
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-02 09:17:48 -0700

    Added FileReader for displaying a image preview of the file laoded

--- client/src/components/FileUploader.js
+++ client/src/components/FileUploader.js
@@ -1,24 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ErrorMsg from "./ErrorMsg";
 
 const FileUploader = () => {
-  const [imgFile, setImgFile] = useState(null);
+  const [imgFile, setImgFile] = useState("");
   const [error, setError] = useState("");
 
+  useEffect(() => {}, [imgFile]);
+
   const handleChange = (e) => {
+    e.preventDefault();
+
+    const reader = new FileReader();
+    console.log("reader", reader);
+
     let selectedFile = e.target.files[0];
 
     const acceptedTypes = ["image/jpeg", "image/png", "image/gif"];
 
     if (selectedFile && acceptedTypes.includes(selectedFile.type)) {
-      setImgFile(selectedFile);
-      console.log("Accepted", selectedFile);
+      reader.addEventListener("load", () => {
+        setImgFile(reader.result);
+        console.log("reader img", reader.result);
+      });
+      reader.readAsDataURL(selectedFile);
+      // setImgFile(selectedFile);
+      console.log("imgFile", imgFile);
+      // console.log("SelectedFile", selectedFile);
+      setError("");
     } else {
       setImgFile(null);
       setError(
         `Invalid filetype: Please select a file with type "jpeg", "jpg", "gif", or "png"`
       );
-      console.log("Not Allowed filetype", selectedFile);
     }
   };
 
@@ -32,7 +45,9 @@ const FileUploader = () => {
           onChange={handleChange}
         />
         {error && <ErrorMsg msg={error} />}
-        <div className="file-display"></div>
+        <div className="file-image-display">
+          {imgFile && <img src={imgFile} alt={imgFile} />}
+        </div>
       </form>
     </div>
   );
