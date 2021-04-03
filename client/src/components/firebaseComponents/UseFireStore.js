543271afc34c230e5ca15716a7411c3bfc07ad28
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-03 11:43:26 -0700

    Added useEffect to retrieve documents and push to an array while ordering by createdAt in descending order

--- client/src/components/firebaseComponents/UseFireStore.js
+++ client/src/components/firebaseComponents/UseFireStore.js
@@ -3,7 +3,21 @@ import ErrorMsg from "../ErrorMsg";
 import { appStorage, appFireStore, timestamp } from "../../firebase/config";
 
 const UseFireStore = (collection) => {
-  return <div></div>;
+  const [firestoreDocs, setFirestoreDocs] = useState([]);
+
+  useEffect(() => {
+    appFireStore.collection(collection).orderBy('createdAt', 'desc').onSnapshot((snap) => {
+      let documents = [];
+      snap.forEach((doc) => {
+        documents.push({ ...doc.data(), id: doc.id });
+      });
+
+      setFirestoreDocs(documents);
+      console.log(documents);
+    });
+  }, [collection]);
+
+  return { firestoreDocs };
 };
 
 export default UseFireStore;
