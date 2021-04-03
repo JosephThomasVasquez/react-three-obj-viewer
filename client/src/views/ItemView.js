ca95b269e98902b9b68573240f16cb44d4e61eec
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-03 13:24:18 -0700

    Added UseFirestore to ItemView

--- client/src/views/ItemView.js
+++ client/src/views/ItemView.js
@@ -1,8 +1,9 @@
 import React from "react";
+import UseFireStore from "../components/firebaseComponents/UseFBStorage";
 
 const ItemView = ({ location, match }) => {
   console.log(location, match);
-  return <div className="item-title">{JSON.stringify(match)}</div>;
+  return <div className="item-title">{JSON.stringify(match.params)}</div>;
 };
 
 export default ItemView;
