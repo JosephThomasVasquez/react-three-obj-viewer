346037cb3a825d13ed2ff6f1df96413b8406cbc1
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-02 18:52:15 -0700

    Changed progressbar loading so the bar is updating as the bytesTrasferred changes

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UseFBStorage from "./firebaseComponents/UseFBStorage";
 
 const ProgressBar = ({ file, setImgFile }) => {
   const { url, progress } = UseFBStorage(file);
   console.log("Uploading...", progress);
+
+  useEffect(() => {
+    if (url) {
+      setImgFile(null);
+    }
+  }, [url, setImgFile]);
 
   return <div className="progress-bar" style={{ width: `${progress}%` }}></div>;
 };
--- client/src/components/firebaseComponents/UseFBStorage.js
+++ client/src/components/firebaseComponents/UseFBStorage.js
@@ -18,23 +18,25 @@ const UseFBStorage = (file) => {
     const storageReference = appStorage.ref().child(`images/${file.name}`);
 
     // uploads the file to FireBase storage
-    storageReference
-      .put(file, metaData)
-      .then((snap) => {
+    storageReference.put(file, metaData).on(
+      "state_changed",
+      (snap) => {
         let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
         console.log("totalBytes", snap.bytesTransferred);
         console.log("metaData", snap.metadata);
         setProgress(percentage);
-
-        snap.ref.getDownloadURL().then((url) => {
-          setFileUrl(url);
-          console.log("file url", url);
-        });
-      })
-      .catch((error) => {
+        console.log("percent", percentage);
+      },
+      (error) => {
         setError(error);
         console.log("upload Error", error);
-      });
+      },
+      async () => {
+        const url = await storageReference.getDownloadURL();
+        setFileUrl(url);
+        console.log("file url", url);
+      }
+    );
   }, [file]);
 
   return { progress, fileUrl, error };
