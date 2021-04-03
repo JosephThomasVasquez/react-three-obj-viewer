3a1b8727dec4d2a9b5135ac1b5e947d53d6dc47e
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-03 14:21:21 -0700

    Added docData state and set state with firestore data retrieved from snap.data()

--- client/src/views/ItemView.js
+++ client/src/views/ItemView.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import { appFireStore } from "../firebase/config";
 
 const ItemView = ({ location, match }) => {
+  const [docData, setDocData] = useState({
+    fileUrl: "",
+    createdAt: ''
+  });
+
   console.log("location", location);
   console.log("match", match.params.id);
 
@@ -11,14 +16,22 @@ const ItemView = ({ location, match }) => {
     .collection("images")
     .doc(`${match.params.id}`);
 
-  const getData = docReference.get().then((snap) => {
-    console.log("snap", snap.data());
-  });
+  useEffect(() => {
+    docReference.get().then((snap) => {
+      console.log("snap", snap.data());
+
+      setDocData({ ...snap.data(), fileUrl: snap.data().fileUrl });
+      console.log('Doc Data', docData)
+
+    });
+  }, [match]);
 
   return (
     <div>
       <Header />
-      <div className="item-title">{JSON.stringify(match.params)}</div>
+      <div className="item-title">
+        <img src={docData.fileUrl} alt="" />
+      </div>
     </div>
   );
 };
